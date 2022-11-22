4c96408a10bd0b4d66e114b2299bd64c43edc038
DanilVelichko <velichko.danil@gmail.com>
2022-11-22 10:13:34 +0200

    Task 2 - done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+function createList(option) {
+  const list = document.querySelector("ul");
+  
+  const elements = ingredients.map(option => {
+    const item = document.createElement("li");
+    item.textContent = option;
+    item.classList.add('item');
+    return item;
+  });
+
+  console.log(elements);
+
+  list.append(...elements);
+}
+createList(ingredients);
