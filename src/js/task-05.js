9b7ea209c20667a6a058c8953fef7ff64c1daa04
DanilVelichko <velichko.danil@gmail.com>
2022-11-29 11:03:43 +0100

    task 05 - done

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,7 @@ const textInput = document.querySelector("input#name-input");
 const output = document.querySelector("span#name-output");
 
 textInput.addEventListener("input", (event) => {
-    if (output.textContent === " ") {
+    if (event.currentTarget.value === "") {
         output.textContent = 'Anonymous';
     }
     else {
