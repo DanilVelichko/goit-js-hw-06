cd004259fb5cad0dea7f1e09a2345abc676791ab
DanilVelichko <velichko.danil@gmail.com>
2022-11-30 15:50:01 +0100

    09 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector('button.change-color');
+const showColor = document.querySelector('span.color');
+const body = document.querySelector('body');
+
+changeColor.addEventListener('click', () => {
+   body.style.backgroundColor = getRandomHexColor();
+  showColor.textContent = body.style.backgroundColor;
+});
